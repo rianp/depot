626e4898f7bdeea4c51aeee6b9bcbd96c43fbac2
Ryan Pickell <rianchristopher@gmail.com>
2022-04-09 22:13:51 -0700

    added navbar

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ApplicationRecord
+    validates :title, :description, :image_url, presence: true
+    validates :title, uniqueness: true
+
+    validates :image_url, allow_blank: true, format: { 
+              with: %r{\.(gif|jpg|png)\z}i,
+              message: 'must be a URL for GIF, JPG or PNG image.'
+    }
 end
